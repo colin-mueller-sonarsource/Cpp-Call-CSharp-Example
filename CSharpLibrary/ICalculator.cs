6ca0019e6bf4fa60c8f63e9b1adc9e21c00c4f1b
John Wang <john29917958@gmail.com>
2016-07-11 16:12:58 +0800

    Add C# library interface

---
+++ CSharpLibrary/ICalculator.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharpLibrary
+{
+    public interface ICalculator
+    {
+        int Add(int a, int b);
+        int Minus(int a, int b);
+        int Multiply(int a, int b);
+        int Divide(int a, int b);
+        void PrintNumber(int number);
+    }
+}
